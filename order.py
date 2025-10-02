b8ee15a9dcaaf18c0b11c24a17a6b16591c774d3
sairam-somisetti <somisettisairam@gmail.com>
2025-10-02 22:28:09 +0530

    Update order.py

--- order.py
+++ order.py
@@ -2,8 +2,9 @@ import csv
 import datetime
 import os
 from inventory import load_products, save_products
-
+# ---it's the file to store sales logs---
 SALES_FILE = 'sales.csv'
+#---it's the directory where all the bill files can be stored---
 BILLS_DIR = 'bills'
 
 def add_to_cart(cart, pid, qty):
@@ -17,8 +18,10 @@ def add_to_cart(cart, pid, qty):
     return False
 
 def generate_bill(cart, discount=0):
+    #---calculating the totals---
     total = sum(item['Price'] * item['Qty'] for item in cart)
     total_after_discount = total * (1 - discount/100)
+    #---preparing bill details---
     now = datetime.datetime.now()
     bill_text = []
     bill_text.append(f'Bill Date: {now.strftime("%Y-%m-%d %H:%M:%S")}')
@@ -31,22 +34,27 @@ def generate_bill(cart, discount=0):
         bill_text.append(f"Discount: {discount}%")
         bill_text.append(f"Total After Discount: {total_after_discount:.2f}")
     bill_text.append('--------------------------------')
+    #---structurd bills data---
     bill_data = {
         'date': now.strftime("%Y-%m-%d"),
         'items': cart,
         'total': total_after_discount
     }
+    #---logs sales record---
     log_sales(cart, now)
     return ''.join(bill_text), bill_data
 
 def save_bill(bill_text, bill_data, format='txt'):
     if not os.path.exists(BILLS_DIR):
         os.makedirs(BILLS_DIR)
+        #---the file name with time stamp for unique quality---
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = f"{BILLS_DIR}/bill_{timestamp}.{format}"
+    #---saving as text file---
     if format == 'txt':
         with open(filename, 'w') as f:
             f.write(bill_text)
+            #---saving as CSV file---
     elif format == 'csv':
         with open(filename, 'w', newline='') as f:
             writer = csv.writer(f)
@@ -60,4 +68,5 @@ def log_sales(cart, now):
     with open(SALES_FILE, 'a', newline='') as file:
         writer = csv.writer(file)
         for item in cart:
+
             writer.writerow([now.strftime("%Y-%m-%d"), item['ID'], item['Qty'], item['Price'], item['Price']*item['Qty']])
