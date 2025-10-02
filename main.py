db098813608a7a1634787d613e494cadffcd5279
sairam-somisetti <somisettisairam@gmail.com>
2025-10-02 22:17:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#importing all the necessary functions from various modules.
 from inventory import add_product, update_product, delete_product, search_product
 from inventory import load_products
 from order import add_to_cart, generate_bill, save_bill
@@ -5,6 +6,7 @@ from reports import daily_sales_report, low_stock_report
 
 def main_menu():
     while True:
+        # displaying the main menu with different options.
         print("1. Add Product")
         print("2. Update Product")
         print("3. Delete Product")
@@ -14,13 +16,16 @@ def main_menu():
         print("7. Daily Sales Report")
         print("8. Low Stock Products")
         print("9. Exit")
+        # getting user choices.
         choice = input("Pick an option: ")
+        #adding new product
         if choice == '1':
             pid = input("Product ID: ")
             name = input("Name: ")
             price = float(input("Price: "))
             stock = int(input("Stock: "))
             add_product(pid, name, price, stock)
+            #---update product details---
         elif choice == '2':
             pid = input("Product ID to update: ")
             key = input("Field to update (Name/Price/Stock): ")
@@ -30,16 +35,20 @@ def main_menu():
             elif key == "Stock":
                 value = int(value)
             update_product(pid, **{key: value})
+            #---deleting a new product---
         elif choice == '3':
             pid = input("Product ID to delete: ")
             delete_product(pid)
+            #---searching for a product by it's ID or name.
         elif choice == '4':
             keyword = input("Enter product ID or name: ")
             results = search_product(keyword)
             print(results)
+            #---display of all products---
         elif choice == '5':
             for prod in load_products():
                 print(prod)
+                #---creating a new order---
         elif choice == '6':
             cart = []
             while True:
@@ -49,27 +58,34 @@ def main_menu():
                 qty = int(input("Quantity: "))
                 if not add_to_cart(cart, pid, qty):
                     print("Not enough stock or invalid product ID.")
+                    #apply the discount if given.
             disc = input("Discount (%) (blank for 0): ")
             discount = float(disc) if disc else 0
+            #generate and display bill
             bill_text, bill_data = generate_bill(cart, discount)
             print(bill_text)
+            #saving bill in chosen format
             fmt = input("Save bill as txt or csv? [txt/csv]: ")
             save_bill(bill_text, bill_data, fmt)
+            #---generating the daily sales reports---.
         elif choice == '7':
             date = input("Enter date (YYYY-MM-DD): ")
             total, items = daily_sales_report(date)
             print(f"Total Sales: {total}")
             for item in items:
                 print(item)
+                #---showing all products with low stock---
         elif choice == '8':
             threshold = int(input("Enter stock threshold: "))
             low_stock = low_stock_report(threshold)
             print("Low Stock Products:")
             for prod in low_stock:
                 print(prod)
+                # ---exit from the program---
         elif choice == '9':
             break
-
+# ---entry point of the program or function call---
 if __name__ == '__main__':
     main_menu()
+
     
